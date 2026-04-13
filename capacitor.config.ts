0f456314250e75ef2d843d46e409826d8da7f2b7
Lucky-Arya07 <luckybagas681@gmail.com>
2026-04-13 07:43:52 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'LkMaxzBlock',
+  webDir: 'www'
+};
+
+export default config;
